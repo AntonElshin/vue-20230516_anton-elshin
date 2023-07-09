6b9d9f5a35776c93e7a4c24583125d5d0aca906a
Anton Elshin <anton.elshin@mail.ru>
2023-07-09 16:29:42 +0300

    Solve counter button complete

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,32 @@
 <template>
-  <button></button>
+  <button @click="() => localCount++">{{localCount}}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+
+  props: {
+    count: Object,
+  },
+
+  data() {
+    return {
+      localCount: this.count || 0,
+    };
+  },
+
+  watch: {
+    count: {
+      handler() {
+        this.localCount = this.count;
+      }
+    },
+    localCount: {
+      handler() {
+        this.$emit('update:count', this.localCount);
+      },
+    },
+  },
 };
 </script>
